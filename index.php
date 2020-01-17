fca0fc1d391ee2cbda7b54e8a1ac81e781da761a
ethernetcat <ethernetcat@protonmail.com>
2020-01-18 03:46:31 +0900

    Add processing to read index page

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+$directory = $_SERVER['REQUEST_URI'];
+$directories = explode("/", $directory);
 
+// Load initialising the page
+require('core/init.php');
+
+// Load page content
+if(empty($directories[0])) {
+  $page = 'pages/index.php';
+}
+
+require($page);
 ?>
